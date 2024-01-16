2d2fdac16b733ce817c6751f864bd37fbfa2fa87
shashi <shashi.kamle99@gmail.com>
2024-01-16 17:02:55 +0530

    added files

--- workspace.tf
+++ workspace.tf
@@ -23,8 +23,8 @@ resource "databricks_mws_workspaces" "this" {
   account_id     = var.databricks_account_id
   aws_region     = var.region
   workspace_name = var.workspace_name
-  credentials_id           = data.aws_cloudformation_stack.cft.CredentialsId
-  storage_configuration_id = data.aws_cloudformation_stack.cft1.StorageConfigId
+  credentials_id           = data.aws_cloudformation_stack.cft.CredentialsId.value
+  storage_configuration_id = data.aws_cloudformation_stack.cft1.StorageConfigId.value
   # network_id               = databricks_mws_networks.this.network_id
 
   # token {
