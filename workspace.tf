251115d7c0f551d0ae398458f52cbbcce267f015
shashi <shashi.kamle99@gmail.com>
2024-01-16 19:19:38 +0530

    added files

--- workspace.tf
+++ workspace.tf
@@ -8,13 +8,13 @@
 # }
 
 
-data "aws_cloudformation_export" "this" {
-  name = "Credential ID"
-}
+# data "aws_cloudformation_export" "this" {
+#   name = "Credential ID"
+# }
 
-data "aws_cloudformation_export" "this1" {
-  name = "Storage configuration ID"
-}
+# data "aws_cloudformation_export" "this1" {
+#   name = "Storage configuration ID"
+# }
 
 
 
@@ -26,13 +26,18 @@ data "aws_cloudformation_export" "this1" {
 # data "databricks_mws_credentials" "this" {}
 
 
+data "aws_cloudformation_stack" "this" {
+  name = "databricks-workspace-stack-eb289"
+}
+
+
 resource "databricks_mws_workspaces" "this" {
   provider       = databricks.mws
   account_id     = var.databricks_account_id
   aws_region     = var.region
   workspace_name = var.workspace_name
-  # credentials_id           = data.aws_cloudformation_export.this.value
-  # storage_configuration_id = data.aws_cloudformation_export.this1.value
+  credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
+  storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
   # network_id               = ""
 
   # token {
