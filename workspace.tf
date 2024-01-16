08165a7e4facbc9e3fd7dd3f6142f3461a65366f
shashi <shashi.kamle99@gmail.com>
2024-01-16 23:38:25 +0530

    changes

--- workspace.tf
+++ workspace.tf
@@ -13,7 +13,7 @@ resource "databricks_mws_workspaces" "this" {
   workspace_name = var.workspace_name
   credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
-  network_id               = databricks_mws_networks.this.network_id
+  # network_id               = databricks_mws_networks.this.network_id
 
 
   token {
