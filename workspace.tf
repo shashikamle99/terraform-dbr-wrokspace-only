92efdeb28afbe984eda3307ee1c037a46ef3fa35
shashi <shashi.kamle99@gmail.com>
2024-01-16 18:39:11 +0530

    added files

--- workspace.tf
+++ workspace.tf
@@ -1,17 +1,26 @@
-data "databricks_mws_credentials" "this" {}
+# data "databricks_mws_credentials" "this" {}
 
-data "databricks_mws_storage_configurations" "this" {}
-
-
+# data "databricks_mws_storage_configurations" "this" {}
 
 # output "databricks_account_id" {
 #   value = data.aws_ssm_parameter.dbr_account_id.value
 #   sensitive = true
 # }
 
+
+data "aws_cloudformation_export" "this" {
+  name = "CredentialsId"
+}
+
+data "aws_cloudformation_export" "this1" {
+  name = "StorageConfigId"
+}
+
+
+
 locals {
-    credentials_id  = data.databricks_mws_credentials.this.id
-    storage_configuration_id  = data.databricks_mws_storage_configurations.this.id
+    credentials_id  = data.aws_cloudformation_export.this.value
+    storage_configuration_id  = data.aws_cloudformation_export.this1.value
 }
 
 
