b801d395fa0370ea0939c2ea19e39509470bea99
shashi <shashi.kamle99@gmail.com>
2024-01-16 23:33:48 +0530

    changes

--- workspace.tf
+++ workspace.tf
@@ -2,11 +2,7 @@ data "aws_cloudformation_stack" "this" {
   name = "databricks-workspace-stack-bfe39"
 }
 
-data "aws_vpc" "this" {
-  tags = {
-    Name = "databricks-WorkerEnvId(workerenv-8281225760377684-ed84b6cf-a205-4105-85b5-5131781c9e96)"
-  }
-}
+
 
 
 
@@ -17,7 +13,8 @@ resource "databricks_mws_workspaces" "this" {
   workspace_name = var.workspace_name
   credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
-  network_id               = data.aws_vpc.this.id
+  network_id               = databricks_mws_networks.this.network_id
+
 
   token {
     comment = "Terraform"
