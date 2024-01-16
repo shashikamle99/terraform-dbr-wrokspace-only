97306748e48e3ff98ce4186c8372565f568e1ba1
shashi <shashi.kamle99@gmail.com>
2024-01-16 16:26:47 +0530

    changes

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_cloudformation_stack" "cft" {
-  name = "databricks-workspace-stack-eb289"
-}
+# data "aws_cloudformation_stack" "cft" {
+#   name = "databricks-workspace-stack-eb289"
+# }
 
