ef1c89bf2db62e846687a2662c05097dd0b2bdf7
shashi <shashi.kamle99@gmail.com>
2024-01-16 22:32:07 +0530

    changes

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "aws_ssm_parameter" "client_id" {
-  name = "dbr_client_id"
+  name = "client_id"
 }
 
 data "aws_ssm_parameter" "client_secret" {
-  name = "dbr_client_secret"
+  name = "client_secret"
 }
 
 data "aws_ssm_parameter" "dbr_account_id" {
