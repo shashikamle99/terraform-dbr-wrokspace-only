327fc4de3fdca2fffcdec5e20cb037ff2de7f929
shashi <shashi.kamle99@gmail.com>
2024-01-16 21:01:28 +0530

    changes

--- workspace.tf
+++ workspace.tf
@@ -32,11 +32,9 @@ data "aws_cloudformation_stack" "this" {
 
 
 
-data "aws_vpcs" "this" {
-  tags = {
-    name = "databricks-WorkerEnvId(workerenv-4871281226906886-d3abd75f-c4a9-465c-8abf-1dca4739c480)"
-  }
-}
+# data "aws_vpcs" "this" {
+#     name = "databricks-WorkerEnvId(workerenv-4871281226906886-d3abd75f-c4a9-465c-8abf-1dca4739c480)"
+# }
 
 resource "databricks_mws_workspaces" "this" {
   provider       = databricks.mws
@@ -45,7 +43,7 @@ resource "databricks_mws_workspaces" "this" {
   workspace_name = var.workspace_name
   credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
-  network_id               = data.aws_vpcs.this.id
+  network_id               = "vpc-0a4de24baa0911126"
 
   token {
     comment = "Terraform"
