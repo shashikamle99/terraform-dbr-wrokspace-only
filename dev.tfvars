95c143033433598abfbc2d84ab5c3a469e32bf93
shashi <shashi.kamle99@gmail.com>
2024-01-16 16:42:14 +0530

    added files

---
+++ dev.tfvars
@@ -0,0 +1,9 @@
+databricks_account_id = "7862c1b2-b196-453e-a9c2-121a7ed7b36d"
+
+client_id = "c323b76e-04c6-4aaf-b469-42527453f3a5"
+
+client_secret = "dose6bfad99d5e10ab1eb4a5528dfd76ce95"
+
+region = "us-west-1"
+
+workspace_name = "dev-terra-ws"
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "databricks_account_id" {}
+
+variable "workspace_name" {}
+
+# variable "tags" {
+#   default = {}
+# }
+
+# variable "cidr_block" {
+#   default = "10.4.0.0/16"
+# }
+
+variable "region" {
+  default = "us-west-1"
+}
+
+# resource "random_string" "naming" {
+#   special = false
+#   upper   = false
+#   length  = 6
+# }
+
+# locals {
+#   prefix = "demo${random_string.naming.result}"
+# }
