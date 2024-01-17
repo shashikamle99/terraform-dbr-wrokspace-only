f529786e5956d22020b05c4e60e20aeb2f824a8c
shashi <shashi.kamle99@gmail.com>
2024-01-17 17:21:05 +0530

    changes

--- variable.tf
+++ variable.tf
@@ -14,5 +14,5 @@ variable "region" {
 variable "cidr_block" {
   description = "The source CIDR block to allow traffic from"
   default     = ["10.43.1.0/24","10.43.2.0/24"]
-  type        = "list(string)"
+  type        = list
 }
