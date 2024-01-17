1e2955d0a88487babef3865a55bc10efc9f5138e
shashi <shashi.kamle99@gmail.com>
2024-01-17 19:48:50 +0530

    changes

--- network.tf
+++ network.tf
@@ -25,26 +25,26 @@ data "aws_vpc" "this" {
 
 
 
-data "aws_subnets" "this" {
-  filter {
-    name   = "tag:Name"
-    values = ["dbr-pri-subnet","nat-gateway-subnet"] # insert values here
-  }
-}
 
 
+# data "aws_subnets" "this" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["dbr-pri-subnet","nat-gateway-subnet"] # insert values here
+#   }
+# }
 
 
-resource "aws_security_group" "sg" {
-    vpc_id = data.aws_vpc.this.id
-}
+# resource "aws_security_group" "sg" {
+#     vpc_id = data.aws_vpc.this.id
+# }
 
 
-resource "databricks_mws_networks" "this" {
-  provider           = databricks.mws
-  account_id         = local.databricks_account_id
-  network_name       = "dev-network"
-  security_group_ids = [aws_security_group.sg.id]
-  subnet_ids         = data.aws_subnets.this.ids
-  vpc_id             = data.aws_vpc.this.id
-}
+# resource "databricks_mws_networks" "this" {
+#   provider           = databricks.mws
+#   account_id         = local.databricks_account_id
+#   network_name       = "dev-network"
+#   security_group_ids = [aws_security_group.sg.id]
+#   subnet_ids         = data.aws_subnets.this.ids
+#   vpc_id             = data.aws_vpc.this.id
+# }
--- workspace.tf
+++ workspace.tf
@@ -13,7 +13,7 @@ resource "databricks_mws_workspaces" "this" {
   workspace_name = var.workspace_name
   credentials_id           = databricks_mws_credentials.this.credentials_id
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
-  network_id               = databricks_mws_networks.this.network_id
+  network_id               = "44408f46-7b74-4f72-8908-f058600bad11"
 
   token {
     comment = "Terraform"
