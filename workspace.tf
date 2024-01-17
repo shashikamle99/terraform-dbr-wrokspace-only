63abe10a02ec548ec1cf89d7d8d85dc7ae1ca226
shashi <shashi.kamle99@gmail.com>
2024-01-17 20:23:48 +0530

    changes

--- workspace.tf
+++ workspace.tf
@@ -13,7 +13,7 @@ resource "databricks_mws_workspaces" "this" {
   workspace_name = var.workspace_name
   credentials_id           = databricks_mws_credentials.this.credentials_id
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
-  network_id               = "vpc-053052015ea4fb28b"
+  network_id               = "my-network-config"
 
   token {
     comment = "Terraform"
