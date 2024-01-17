9d8f7838916d0588804e712c93fb6602698d4eed
shashi <shashi.kamle99@gmail.com>
2024-01-17 08:04:43 +0530

    changes

--- workspace.tf
+++ workspace.tf
@@ -1,7 +1,7 @@
 
 
 data "aws_cloudformation_stack" "this" {
-  name = "databricks-workspace-stack-bfe39"
+  name = "databricks-workspace-stack-2a4a3"
 }
 
 
@@ -11,6 +11,20 @@ resource "databricks_mws_workspaces" "this" {
   account_id     = local.databricks_account_id
   aws_region     = var.region
   workspace_name = var.workspace_name
+  credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
+  storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
+
+  token {
+    comment = "Terraform"
+  }
+}
+
+
+resource "databricks_mws_workspaces" "this" {
+  provider       = databricks.mws
+  account_id     = local.databricks_account_id
+  aws_region     = var.region
+  workspace_name = dbr-dev-ws-2
   credentials_id           = data.aws_cloudformation_stack.this.outputs["CredentialsId"]
   storage_configuration_id = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
 
