a1e09fa630c65c35e03e791222623d55cf1c57e7
shashi <shashi.kamle99@gmail.com>
2024-01-18 11:22:46 +0530

    changes

--- output.tf
+++ output.tf
@@ -1,4 +1,12 @@
 output "mws_network_id" {
   value = databricks_mws_networks.this.network_id
- # sensitive = true
 }
+
+output "mws_credential_id" {
+  value = databricks_mws_credentials.this.credentials_id
+}  
+
+
+output "mws_storage_configuration_id" {
+  value = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
+}  
