5014dedefbea7eac45997ad223822993573ffcbb
shashi <shashi.kamle99@gmail.com>
2024-01-19 11:17:10 +0530

    update changes

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
 output "mws_network_id" {
-  value = databricks_mws_networks.this.network_id
+  value = databricks_mws_networks.dbr_mws_networks.network_id
 }
 
 output "mws_credential_id" {
-  value = databricks_mws_credentials.this.credentials_id
+  value = databricks_mws_credentials.dbr_mws_cred.credentials_id
 }  
 
 
 output "mws_storage_configuration_id" {
-  value = data.aws_cloudformation_stack.this.outputs["StorageConfigId"]
+  value = data.aws_cloudformation_stack.aws_cf_stack.outputs["StorageConfigId"]
 }  
